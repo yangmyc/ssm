c23276dde5badeb542d9ce62553c851a5aebb2f9
yangmyc <myc1101255053@163.com>
2017-04-28 17:27:16 +0800

    mybatis逆向生成

---
+++ src/main/java/com/myc/test/MGBTest.java
@@ -0,0 +1,18 @@
+package com.myc.test;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MGBTest {
+		public static void main(String[] args) {
+			List<String> warnings = new ArrayList<String>();
+			   boolean overwrite = true;
+			   File configFile = new File("generatorConfig.xml");
+			   ConfigurationParser cp = new ConfigurationParser(warnings);
+			   Configuration config = cp.parseConfiguration(configFile);
+			   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+			   MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+			   myBatisGenerator.generate(null);
+		}
+}
