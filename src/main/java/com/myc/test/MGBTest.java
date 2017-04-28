cfaa0efb9427df46f7dc2fbcb1dc417d3ec131c9
yangmyc <myc1101255053@163.com>
2017-04-28 17:30:35 +0800

    mybatis逆向

--- src/main/java/com/myc/test/MGBTest.java
+++ src/main/java/com/myc/test/MGBTest.java
@@ -4,11 +4,16 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
 public class MGBTest {
-		public static void main(String[] args) {
+		public static void main(String[] args) throws Exception {
 			List<String> warnings = new ArrayList<String>();
 			   boolean overwrite = true;
-			   File configFile = new File("generatorConfig.xml");
+			   File configFile = new File("mgb.xml");
 			   ConfigurationParser cp = new ConfigurationParser(warnings);
 			   Configuration config = cp.parseConfiguration(configFile);
 			   DefaultShellCallback callback = new DefaultShellCallback(overwrite);
